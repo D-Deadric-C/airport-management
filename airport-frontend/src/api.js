aa207a037f624f8be84dec08484ddf0147edca69
Suryansh <ssuryansh_be23@thapar.edu>
2025-11-26 01:53:34 +0530

    Change API base URL to production endpoint

--- airport-frontend/src/api.js
+++ airport-frontend/src/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-const API_BASE = "http://127.0.0.1:5000/api";
+const API_BASE = "https://airport-management-qjgg.onrender.com/api";
 
 export const api = {
   API_BASE,
+};
 
   // ---------- AUTH ----------
   login: (email, password) =>
